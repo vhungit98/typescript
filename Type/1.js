cc347c40a7ca0a5781689af15460d806c656f408
Hung Le <vhungit.it@gmail.com>
2021-09-23 15:15:59 +0700

    a

--- Type/1.js
+++ Type/1.js
@@ -1,41 +1,56 @@
-{
-    console.log("Hello!");
-    // String
-    var string = "Hưng";
-    // Number
-    var number = 1;
-    // Mảng chữ
-    var array_string = ["Hưng 1", "Hưng 2", "Hưng 3"];
-    // Mảng số
-    var array_number = [1, 2, 3];
-    // True / False
-    var boolean = true;
-    // Enum
-    var trangthai = void 0;
-    (function (trangthai) {
-        trangthai["pending"] = "\u0110ang ch\u1EDD";
-        trangthai["done"] = "Xong";
-    })(trangthai || (trangthai = {}));
-    // Tuple: Mảng có thể chứa nhiều kiểu dữ liệu
-    var tuple = ["Hưng", 23];
-    // Any
-    var x = {
-        pending: "Đang chờ",
-        done: "Xong"
-    };
-    // Void: Không chứa giá trị (thường dùng trong function, không return)
-    {
-        function tinhtoan() {
-            console.log("Xin chào!");
-        }
-        tinhtoan();
-    }
-    console.log("Ch\u00E0o " + string);
-    console.log("S\u1ED1 " + number);
-    console.log("M\u1EA3ng ch\u1EEF: " + array_string);
-    console.log("M\u1EA3ng s\u1ED1: " + array_number);
-    console.log("\u0110\u00FAng sai: " + boolean);
-    console.log("Enum: " + trangthai.pending);
-    console.log(tuple);
-    console.log(x);
+// ------------- Kiểu dữ liệu -------------
+console.log("Hello!");
+// String
+var string = "Hưng";
+// Number
+var number = 1;
+// Mảng chữ
+var array_string = ["Hưng 1", "Hưng 2", "Hưng 3"];
+// Mảng số
+var array_number = [1, 2, 3];
+// True / False
+var boolean = true;
+// Enum
+var trangthai;
+(function (trangthai) {
+    trangthai["pending"] = "\u0110ang ch\u1EDD";
+    trangthai["done"] = "Xong";
+})(trangthai || (trangthai = {}));
+// Tuple: Mảng có thể chứa nhiều kiểu dữ liệu
+var tuple = ["Hưng", 23];
+// Any
+var x = {
+    pending: "Đang chờ",
+    done: "Xong"
+};
+// Void: Không chứa giá trị (thường dùng trong function, không return)
+function tinhtoan() {
+    console.log("Xin chào!");
 }
+tinhtoan();
+// console.log(`Chào ${string}`);
+// console.log(`Số ${number}`);
+// console.log(`Mảng chữ: ${array_string}`);
+// console.log(`Mảng số: ${array_number}`);
+// console.log(`Đúng sai: ${boolean}`);
+// console.log(`Enum: ${trangthai.pending}`);
+// console.log(tuple);
+// console.log(x);
+// ------------- Ép kiểu -------------
+// Note: chỉ dùng cho kiểu any
+var kieu1 = "Lê Văn Hưng";
+console.log(kieu1.length); // C1
+console.log(kieu1.length); // C2
+// ------------- 2 cách khai báo anonymous function -------------
+var a = function (x, y) {
+    return "Ch\u00E0o " + y + ", s\u1ED1 " + x;
+};
+var b = function (x, y) {
+    return "Ch\u00E0o " + y + ", s\u1ED1 " + x;
+};
+console.log(b(23, 'Hưng'));
+// ------------- Khai báo nhanh function -------------
+var h = function (x) {
+    return x + 1;
+};
+console.log(h(5));
--- Type/1.ts
+++ Type/1.ts
@@ -1,41 +1,61 @@
-{
-  console.log("Hello!");
-  // String
-  var string: string = "Hưng";
-  // Number
-  var number: number = 1;
-  // Mảng chữ
-  var array_string: string[] = ["Hưng 1", "Hưng 2", "Hưng 3"];
-  // Mảng số
-  var array_number: number[] = [1, 2, 3];
-  // True / False
-  var boolean: boolean = true;
-  // Enum
-  enum trangthai {
-    pending = "Đang chờ",
-    done = "Xong",
-  }
-  // Tuple: Mảng có thể chứa nhiều kiểu dữ liệu
-  var tuple: [string, number] = ["Hưng", 23];
-  // Any
-  var x: any = {
-    pending: "Đang chờ",
-    done: "Xong",
-  };
-  // Void: Không chứa giá trị (thường dùng trong function, không return)
-  {
-    function tinhtoan(): void {
-      console.log("Xin chào!");
-    }
-    tinhtoan();
-  }
-
-  console.log(`Chào ${string}`);
-  console.log(`Số ${number}`);
-  console.log(`Mảng chữ: ${array_string}`);
-  console.log(`Mảng số: ${array_number}`);
-  console.log(`Đúng sai: ${boolean}`);
-  console.log(`Enum: ${trangthai.pending}`);
-  console.log(tuple);
-  console.log(x);
+// ------------- Kiểu dữ liệu -------------
+console.log("Hello!");
+// String
+var string: string = "Hưng";
+// Number
+var number: number = 1;
+// Mảng chữ
+var array_string: string[] = ["Hưng 1", "Hưng 2", "Hưng 3"];
+// Mảng số
+var array_number: number[] = [1, 2, 3];
+// True / False
+var boolean: boolean = true;
+// Enum
+enum trangthai {
+  pending = "Đang chờ",
+  done = "Xong",
 }
+// Tuple: Mảng có thể chứa nhiều kiểu dữ liệu
+var tuple: [string, number] = ["Hưng", 23];
+// Any
+var x: any = {
+  pending: "Đang chờ",
+  done: "Xong",
+};
+// Void: Không chứa giá trị (thường dùng trong function, không return)
+function tinhtoan(): void {
+  console.log("Xin chào!");
+}
+tinhtoan();
+
+// console.log(`Chào ${string}`);
+// console.log(`Số ${number}`);
+// console.log(`Mảng chữ: ${array_string}`);
+// console.log(`Mảng số: ${array_number}`);
+// console.log(`Đúng sai: ${boolean}`);
+// console.log(`Enum: ${trangthai.pending}`);
+// console.log(tuple);
+// console.log(x);
+
+// ------------- Ép kiểu -------------
+// Note: chỉ dùng cho kiểu any
+var kieu1: any = "Lê Văn Hưng";
+console.log((<string>kieu1).length); // C1
+console.log((kieu1 as string).length); // C2
+
+// ------------- 2 cách khai báo anonymous function -------------
+var a = function (x: number, y: string): string {
+  return `Chào ${y}, số ${x}`;
+}
+var b: (x: number, y: string) => string = function (x, y) {
+  return `Chào ${y}, số ${x}`;
+}
+console.log(b(23, 'Hưng'));
+
+// ------------- Khai báo nhanh function -------------
+var h = (x: number): number => {
+  return x + 1;
+}
+console.log(h(5));
+
+
